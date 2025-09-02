39f9fb7d54b8a43dbc4cd390c47f4700834e66fd
e-zaline <62605130+e-zaline@users.noreply.github.com>
2025-09-02 17:16:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ fig.add_trace(
         x=month_labels,
         y=[str(year) for year in years],
         colorscale=[[0, "red"], [1, "green"]],
-        colorbar=dict(title="Legend: number of hours colorscale"),
+        colorbar=dict(title="Colorscale: number of hours"),
         hoverongaps=False,
         hovertemplate="Date: %{x} %{y}<br>Number of hours: %{z}<extra></extra>",
         showscale=True,
